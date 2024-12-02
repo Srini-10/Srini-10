93b6a21f36991d662dac9043f9e3644f4ed319bb
Srinivasan P <136215579+Srini-10@users.noreply.github.com>
2024-12-03 02:14:58 +0530

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,28 +1,11 @@
 import React from "react";
-import "./Style.css";
+import { Spinner } from "@nextui-org/react";
 
 const Loading = () => {
   return (
     <>
-      <div className="loading-container">
-        <div className="outer-circle">
-          <div className="inner-circle flex align-middle items-center justify-center">
-            <p className="m-auto justify-center text-center align-middle mt-[38px] text-[10px] ml-[13px] flex gap-2">
-              Loading
-            </p>
-            <div className="col-3 ml-[45px] absolute">
-              <div className="snippet" data-title="dot-pulse">
-                <div className="stage">
-                  <div className="dot-pulse"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+      <div className="w-full h-screen flex items-center justify-center">
+        <Spinner color="default" />
       </div>
     </>
   );
